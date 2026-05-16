4c5a3bbf55c814223f9358ee0a9c9f258f4f8583
vembry <airstrip99@gmail.com>
2026-05-16 07:59:14 +0000

    chore(api,web): add test code

--- api/main.go
+++ api/main.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
 	"log"
 	"net/http"
+	"os"
 )
 
 func main() {
@@ -19,14 +21,46 @@ func main() {
 		w.Write(raw)
 	})
 
+	mux.HandleFunc("GET /f", func(w http.ResponseWriter, r *http.Request) {
+		entries, err := os.ReadDir("/mounts/sample directory")
+		if err != nil {
+			raw, _ := json.Marshal(map[string]interface{}{
+				"message": "failed to browse directory",
+				"error":   err.Error(),
+			})
+			w.WriteHeader(http.StatusInternalServerError)
+			w.Write(raw)
+			return
+		}
+
+		entriesOut := []string{}
+		for _, entry := range entries {
+			fmt.Println("Name:", entry.Name())
+			fmt.Println("Is Directory:", entry.IsDir())
+			fmt.Println("-----------------")
+			entriesOut = append(entriesOut, entry.Name())
+		}
+
+		raw, _ := json.Marshal(map[string]interface{}{
+			// "path":    pathQuery,
+			"entries": entriesOut,
+		})
+
+		w.WriteHeader(http.StatusOK)
+		w.Write(raw)
+	})
+
 	// construct server
 	httpserver := http.Server{
 		Addr:    ":80",
 		Handler: mux,
 	}
 
+	log.SetOutput(os.Stdout)
+	log.Printf("starting server...")
 	// start server
 	if err := httpserver.ListenAndServe(); err != nil {
 		log.Fatalf("error on starting server. err=%v", err)
 	}
+	log.Printf("shutting down server...")
 }
---
+++ web/src/routes/[slug]/f/+page.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+	let { params } = $props();
+</script>
+
+token = {params.slug}
+
+<h1>directory page</h1>
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi perferendis animi impedit cumque sed cum cupiditate, blanditiis pariatur non illum praesentium rem expedita nesciunt dolorum aperiam. Pariatur perferendis dignissimos saepe!</p>
