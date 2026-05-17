ce44e1cf7734d2b85553d98011375e7dc5d5c0dc
vembry <airstrip99@gmail.com>
2026-05-17 17:35:38 +0000

    chore(api): implement directory browsing

--- api/main.go
+++ api/main.go
@@ -6,7 +6,13 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"strings"
 )
+
+type PathEntry struct {
+	Path        string `json:"path"`
+	IsDirectory bool   `json:"is_directory"`
+}
 
 func main() {
 	mux := http.NewServeMux()
@@ -14,15 +20,24 @@ func main() {
 	// handler
 	mux.HandleFunc("GET /test", func(w http.ResponseWriter, r *http.Request) {
 		raw, _ := json.Marshal(map[string]interface{}{
-			"message":  "ok",
-			"message1": "ok1",
+			"message": "ok",
 		})
 		w.WriteHeader(http.StatusOK)
 		w.Write(raw)
 	})
 
 	mux.HandleFunc("GET /f", func(w http.ResponseWriter, r *http.Request) {
-		entries, err := os.ReadDir("/mounts/sample directory")
+		pathQuery := r.URL.Query().Get("d")
+
+		fullPath := "/mounts"
+		paths := []string{}
+		if len(strings.TrimSpace(pathQuery)) > 0 {
+			fullPath = fmt.Sprintf("%s/%s", fullPath, pathQuery)
+			paths = strings.Split(pathQuery, "/")
+		}
+
+		// browse directory
+		entries, err := os.ReadDir(fullPath)
 		if err != nil {
 			raw, _ := json.Marshal(map[string]interface{}{
 				"message": "failed to browse directory",
@@ -33,16 +48,17 @@ func main() {
 			return
 		}
 
-		entriesOut := []string{}
+		// construct response
+		entriesOut := []PathEntry{}
 		for _, entry := range entries {
-			fmt.Println("Name:", entry.Name())
-			fmt.Println("Is Directory:", entry.IsDir())
-			fmt.Println("-----------------")
-			entriesOut = append(entriesOut, entry.Name())
+			entriesOut = append(entriesOut, PathEntry{
+				Path:        entry.Name(),
+				IsDirectory: entry.IsDir(),
+			})
 		}
 
 		raw, _ := json.Marshal(map[string]interface{}{
-			// "path":    pathQuery,
+			"paths":   paths,
 			"entries": entriesOut,
 		})
 
